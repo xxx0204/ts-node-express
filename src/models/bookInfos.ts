024f3a3b8cb098c99747e20d95ccf10d80a56072
wangxuz <wangxuz@chanjet.com>
2023-06-03 18:36:11 +0800

    修改数据库结构

--- src/models/bookInfos.ts
+++ src/models/bookInfos.ts
@@ -2,10 +2,10 @@ import * as Knex from 'knex';
 
 export class BookInfos {
   getBookInfos(db: Knex) {
-    return db('books_info').select('id', 'cover', 'book_origin_id', 'size', 'introduce', 'word_num', 'recite_user_num', 'key', 'title', 'offlinedata', 'version', 'tags');
+    return db('books_info').select('id', 'cover', 'book_origin_id', 'size', 'introduce', 'word_num', 'recite_user_num', 'key', 'title', 'offlinedata', 'version', 'tags').whereRaw('books_info.key not like "%luan%"');
   }
 
   getAllKeys(db: Knex, id: number) {
-    return db('books_info').select('id', 'key', 'title', 'offlinedata').where('id', '>=', id);
+    return db('books_info').select('id', 'key', 'title', 'offlinedata').where('id', '>=', id).whereRaw('books_info.key not like "%luan%"');
   }
 }
--- src/models/words.ts
+++ src/models/words.ts
@@ -2,6 +2,10 @@ import * as Knex from 'knex';
 
 export class Words {
   getWords(db: Knex, bookId, word) {
-    return db('full_words').select('*').where('bookId', bookId).where('headWord', word);
+    try {
+      return db(bookId).select('*').where('headWord', word);
+    } catch (error) {
+      return db('full_words').select('*').where('bookId', bookId).where('headWord', word);
+    }
   }
 }
